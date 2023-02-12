6587800e07c37d55b67f6aace1a802a38039e5ad
John Ingle <ingle.john.w@gmail.com>
2023-02-11 18:23:54 -0600

    Basic djot-to-html SSG working.

---
+++ build.ts
@@ -0,0 +1,33 @@
+import djot from "npm:@djot/djot";
+import config from "./config.json" assert {type: "json"};
+import {walk, copy, ensureDir} from "https://deno.land/std/fs/mod.ts";
+import {join} from "https://deno.land/std/path/mod.ts";
+
+const tmpl = await Deno.readTextFile(config.htmlTemplate);
+await Deno.remove(config.htmlRoot, {recursive: true});
+await ensureDir(config.htmlRoot);
+
+for await (const djotFile of walk(config.djotRoot, {
+    includeDirs: false,
+    exts: ['.djot']
+})) {
+    // transform the djot and replace the template token with it
+    const djotContent = await Deno.readTextFile(djotFile.path);
+    const ast = djot.parse(djotContent, {sourcePositions: false});
+    const html = djot.renderHTML(ast);
+    const final = tmpl.replace(config.contentToken, html);
+
+    // determine output directory and file
+    let outDir = config.htmlRoot;
+    if (djotFile.path !== join(config.djotRoot, 'index.djot').toString())
+        outDir = join(config.htmlRoot, djotFile.name.replace('.djot', ''));
+
+    const outFile = join(outDir, 'index.html');
+
+    // ensure the target output directory for the file exists
+    await ensureDir(outDir);
+    await Deno.writeTextFile(outFile, final);
+}
+
+// copy static directory verbatim
+await copy(join(config.djotRoot, 'static'), join(config.htmlRoot, 'static'), {overwrite: true});
---
+++ readme.md
@@ -0,0 +1,18 @@
+# DjotSSG
+
+An opinionated Deno-based nano-SSG using [Djot.js](https://github.com/jgm/djot.js) instead of Markdown.
+
+## Usage
+
+1. Clone this repository.
+2. Add your .djot files to src/.
+3. Add your static resource to src/static/.
+4. Customize src/template.html as needed.
+5. Run `deno task build`.
+6. Publish contents of site/ to your static website host.
+
+## Features
+
+- [x] Basic djot-to-html conversion working with minimal config and static resource support
+- [x] Pretty URLs with FS routing based on djot file names
+- [ ] Pretty URLs mirroring subdirectories in src/
