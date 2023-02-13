0cf2e3ddd3ffc59823a0f7142764b782a383b83d
John Ingle <ingle.john.w@gmail.com>
2023-02-12 19:30:45 -0600

    Generate pages.json in static output directory containing all transformed pages.

--- build.ts
+++ build.ts
@@ -1,26 +1,42 @@
 import djot from "npm:@djot/djot";
 import config from "./config.json" assert {type: "json"};
 import {walk, WalkEntry, copy, ensureDir} from "https://deno.land/std@0.177.0/fs/mod.ts";
-import {join, dirname} from "https://deno.land/std@0.177.0/path/mod.ts";
+import {join, dirname, normalize} from "https://deno.land/std@0.177.0/path/mod.ts";
 
+const pageList: Array<string> = [];
 const htmlTemplate = await Deno.readTextFile(config.htmlTemplateFile);
-await EmptyHtmlRootDir();
-await CopyStaticDir();
-await WalkDjotFilesAndOutputHtml();
+await empty_html_root_dir();
+await copy_static_dir();
+await walk_djot_files_and_output_html();
 
-async function WalkDjotFilesAndOutputHtml() {
+async function walk_djot_files_and_output_html() {
     for await (const djotFile of walk(config.djotRootDir, {
         includeDirs: false,
         exts: ['.djot']
     })) {
-        const finalHtml = await TransformDjotToHtml(djotFile);
-        const outFile = CalculateOutputFilePath(djotFile);
+        const finalHtml = await transform_djot_to_html(djotFile);
+        const outFile = calculate_output_file_path(djotFile);
         await ensureDir(dirname(outFile));
         await Deno.writeTextFile(outFile, finalHtml);
+        const pagePath = dirname(outFile).replace(config.htmlRootDir, '');
+        pageList.push(pagePath);
     }
 }
 
-async function EmptyHtmlRootDir() {
+await write_json(pageList);
+
+async function write_json(pages: Array<string>) {
+    const outFile = join(config.htmlRootDir, 'static', 'pages.json');
+    const outJson = JSON.stringify(pages);
+
+    try {
+        await Deno.writeTextFile(outFile, outJson);
+    } catch {
+        console.log('Error writing pages.json, continuing.')
+    }
+}
+
+async function empty_html_root_dir() {
     try {
         await Deno.remove(config.htmlRootDir, {recursive: true});
     } catch {
@@ -30,14 +46,14 @@ async function EmptyHtmlRootDir() {
     }
 }
 
-async function CopyStaticDir() {
+async function copy_static_dir() {
     await copy(
         join(config.djotRootDir, 'static'),
         join(config.htmlRootDir, 'static'),
         {overwrite: true});
 }
 
-async function TransformDjotToHtml(djotFile: WalkEntry) {
+async function transform_djot_to_html(djotFile: WalkEntry) {
     const djotContent = await Deno.readTextFile(djotFile.path);
     const ast = djot.parse(djotContent, {sourcePositions: false});
     const html = djot.renderHTML(ast);
@@ -45,7 +61,7 @@ async function TransformDjotToHtml(djotFile: WalkEntry) {
     return final;
 }
 
-function CalculateOutputFilePath(djotFile: WalkEntry) {
+function calculate_output_file_path(djotFile: WalkEntry) {
     let outFile = djotFile.path.replace(config.djotRootDir, config.htmlRootDir);
 
     if (djotFile.name == 'index.djot') {
